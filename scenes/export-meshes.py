be039ab96811f917b0e0fef57096cbfc7ad6668a
Jim McCann (on silkmoth) <ix@tchow.com>
2025-10-30 11:46:44 -0400

    modern scene export -> correct (linear) albedo -> rendering matches blender (also cleaned up a bunch of unused blender files)

--- scenes/export-meshes.py
+++ scenes/export-meshes.py
@@ -2,9 +2,10 @@
 
 #based on 'export-sprites.py' and 'glsprite.py' from TCHOW Rainbow; code used is released into the public domain.
 #Patched for 15-466-f19 to remove non-pnct formats!
+#Patched for 15-466-f20 to merge data all at once (slightly faster)
 
-#Note: Script meant to be executed within blender, as per:
-#blender --background --python export-meshes.py -- <infile.blend>[:collection] <outfile.pnct>
+#Note: Script meant to be executed within blender 4.2.1, as per:
+#blender --background --python export-meshes.py -- [...see below...]
 
 import sys,re
 
@@ -14,7 +15,7 @@ for i in range(0,len(sys.argv)):
 		args = sys.argv[i+1:]
 
 if len(args) != 2:
-	print("\n\nUsage:\nblender --background --python export-meshes.py -- <infile.blend>[:collection] <outfile.pnct>\nExports the meshes referenced by all objects in the specified collection (default: all objects) to a binary blob.\n")
+	print("\n\nUsage:\nblender --background --python export-meshes.py -- <infile.blend[:collection]> <outfile.pnct>\nExports the meshes referenced by all objects in the specified collection(s) (default: all objects) to a binary blob.\n")
 	exit(1)
 
 import bpy
@@ -48,17 +49,34 @@ if collection_name:
 else:
 	collection = bpy.context.scene.collection
 
+
 #meshes to write:
 to_write = set()
+did_collections = set()
 def add_meshes(from_collection):
 	global to_write
+	global did_collections
+	if from_collection in did_collections:
+		return
+	did_collections.add(from_collection)
+
+	if from_collection.name[0] == '_':
+		print("Skipping collection '" + from_collection.name + "' because its name starts with an underscore.")
+		return
+
 	for obj in from_collection.objects:
 		if obj.type == 'MESH':
-			to_write.add(obj.data)
+			if obj.data.name[0] == '_':
+				print("Skipping mesh '" + obj.data.name + "' because its name starts with an underscore.")
+			else:
+				to_write.add(obj.data)
+		if obj.instance_collection:
+			add_meshes(obj.instance_collection)
 	for child in from_collection.children:
 		add_meshes(child)
 
 add_meshes(collection)
+#print("Added meshes from: ", did_collections)
 
 #set all collections visible: (so that meshes can be selected for triangulation)
 def set_visible(layer_collection):
@@ -71,7 +89,7 @@ def set_visible(layer_collection):
 set_visible(bpy.context.view_layer.layer_collection)
 
 #data contains vertex, normal, color, and texture data from the meshes:
-data = b''
+data = []
 
 #strings contains the mesh names:
 strings = b''
@@ -86,6 +104,7 @@ for obj in bpy.data.objects:
 	else:
 		continue
 
+	obj.hide_select = False
 	mesh = obj.data
 	name = mesh.name
 
@@ -111,9 +130,6 @@ for obj in bpy.data.objects:
 	bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
 	bpy.ops.object.mode_set(mode='OBJECT')
 
-	#compute normals (respecting face smoothing):
-	mesh.calc_normals_split()
-
 	#record mesh name, start position and vertex count in the index:
 	name_begin = len(strings)
 	strings += bytes(name, "utf8")
@@ -125,16 +141,22 @@ for obj in bpy.data.objects:
 	#...count will be written below
 
 	colors = None
-	if len(obj.data.vertex_colors) == 0:
+	if len(obj.data.color_attributes) == 0:
 		print("WARNING: trying to export color data, but object '" + name + "' does not have color data; will output 0xffffffff")
 	else:
-		colors = obj.data.vertex_colors.active.data
+		colors = obj.data.color_attributes.active_color;
+		if len(obj.data.color_attributes) != 1:
+			print("WARNING: object '" + name + "' has multiple vertex color layers; only exporting '" + colors.name + "'")
 
 	uvs = None
 	if len(obj.data.uv_layers) == 0:
 		print("WARNING: trying to export texcoord data, but object '" + name + "' does not uv data; will output (0.0, 0.0)")
 	else:
 		uvs = obj.data.uv_layers.active.data
+		if len(obj.data.uv_layers) != 1:
+			print("WARNING: object '" + name + "' has multiple texture coordinate layers; only exporting '" + obj.data.uv_layers.active.name + "'")
+
+	local_data = b''
 
 	#write the mesh triangles:
 	for poly in mesh.polygons:
@@ -144,26 +166,37 @@ for obj in bpy.data.objects:
 			loop = mesh.loops[poly.loop_indices[i]]
 			vertex = mesh.vertices[loop.vertex_index]
 			for x in vertex.co:
-				data += struct.pack('f', x)
+				local_data += struct.pack('f', x)
 			for x in loop.normal:
-				data += struct.pack('f', x)
-			if colors != None:
-				col = colors[poly.loop_indices[i]].color
-				data += struct.pack('BBBB', int(col[0] * 255), int(col[1] * 255), int(col[2] * 255), int(col[3] * 255))
+				local_data += struct.pack('f', x)
+
+			col = None
+			if colors != None and colors.domain == 'POINT':
+				col = colors.data[poly.vertices[i]].color
+			elif colors != None and colors.domain == 'CORNER':
+				col = colors.data[poly.loop_indices[i]].color
 			else:
-				data += struct.pack('BBBB', 255, 255, 255, 255)
+				col = (1.0, 1.0, 1.0, 1.0)
+			local_data += struct.pack('BBBB', int(col[0] * 255), int(col[1] * 255), int(col[2] * 255), int(col[3] * 255))
+
 			if uvs != None:
 				uv = uvs[poly.loop_indices[i]].uv
-				data += struct.pack('ff', uv.x, uv.y)
+				local_data += struct.pack('ff', uv.x, uv.y)
 			else:
-				data += struct.pack('ff', 0, 0)
+				local_data += struct.pack('ff', 0, 0)
+		if len(local_data) > 1000:
+			data.append(local_data)
+			local_data = b''
 	vertex_count += len(mesh.polygons) * 3
+
+	data.append(local_data)
 
 	index += struct.pack('I', vertex_count) #vertex_end
 
+data = b''.join(data)
 
 #check that code created as much data as anticipated:
-assert(vertex_count * (4*3+4*3+4*1+4*2) == len(data))
+assert(vertex_count * (4*3+4*3+1*4+4*2) == len(data))
 
 #write the data chunk and index chunk to an output blob:
 blob = open(outfile, 'wb')
